c2ca679494fe7b3414de6d3bea3c438322fede41
Barry Whiting <bwhiti@msn.com>
2021-11-17 12:27:20 +0000

    Added new Games image for the games page and added compnent template html

--- src/components/Games/Games.tsx
+++ src/components/Games/Games.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import games from '../static/images/games.jpg';
+
+import '../static/scss/splitPageContent.scss';
+
 const Games = (): JSX.Element => {
-  return <h1>Games Page</h1>;
+  const imageWidth = 150;
+  const imageHeight = 113;
+  return <>
+    <img src={games} width={imageWidth} height={imageHeight} />
+    <div>
+      <h2>Games</h2>
+    </div>
+  </>;
 };
 
 export default Games;
